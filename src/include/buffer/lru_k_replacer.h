0d16da79cc3bb335c76aed1d54d05847238f7009
Wesley Chou <3210103790@zju.edu.cn>
2023-06-12 15:42:55 +0800

    update

--- src/include/buffer/lru_k_replacer.h
+++ src/include/buffer/lru_k_replacer.h
@@ -2,44 +2,32 @@
 #define MINISQL_LRUK_REPLACER_H
 
 #include <list>
-#include <mutex>
-#include <unordered_set>
 #include <unordered_map>
 #include <vector>
+#include <cstddef>
+#include <utility>
 
 #include "buffer/replacer.h"
 #include "common/config.h"
 
-using namespace std;
-
-/**
- * LRUReplacer implements the Least Recently Used replacement policy.
- */
 class LRUKReplacer : public Replacer {
  public:
-
   explicit LRUKReplacer(size_t num_pages);
-
   ~LRUKReplacer() override;
 
-  bool Victim(frame_id_t *frame_id) override;
-
+  bool Victim(frame_id_t* frame_id) override;
   void Pin(frame_id_t frame_id) override;
-
   void Unpin(frame_id_t frame_id) override;
-
   size_t Size() override;
 
-private:
-  // add your own private member variables here
-  std::unordered_map<frame_id_t,unsigned int> access_time;
+ private:
+  std::unordered_map<frame_id_t, unsigned int> access_time_;
   std::list<frame_id_t> history_list_;
   std::list<frame_id_t> cache_list_;
   std::unordered_map<frame_id_t, std::list<frame_id_t>::iterator> history_map_;
   std::unordered_map<frame_id_t, std::list<frame_id_t>::iterator> cache_map_;
-
-  std::unordered_map<frame_id_t, bool> is_evictable_;  
-  int64_t k_;
+  std::unordered_map<frame_id_t, bool> is_evictable_;
+  size_t k_;
 };
 
 #endif  // MINISQL_LRUK_REPLACER_H
